fecb7425a6ec1eeb545709f33389c4bff053d900
Darragh King <kingdarragh56@gmail.com>
2019-01-26 18:35:02 +0100

    Present mapview from home router

--- FallenMeteors/Home/HomeRouter.swift
+++ FallenMeteors/Home/HomeRouter.swift
@@ -33,4 +33,26 @@ class HomeRouter: HomeRouterProtocol {
         
         interactor.loadData()
     }
+    
+    private func showMeteorOnMap(meteor: MeteorData) {
+        guard meteor.geoLocation != nil else {return}
+        
+        mapRouter = MapRouter()
+        
+        let mapView = mapRouter!.assembleModule()
+        view.presentViewController(mapView)
+        
+        mapRouter?.showMeteorOnMap(meteor: meteor, completionHandler: { [weak self] in
+            self?.handleDismissalOfMapView()
+        })
+    }
+    
+    private func handleDismissalOfMapView() {
+        view.dismissCurrentChildVC()
+    }
+}
+extension HomeRouter {
+    func didSelectMeteor(meteor: MeteorData) {
+        showMeteorOnMap(meteor: meteor)
+    }
 }
